7a45437eb3b82dae14876124da0fda976e889364
Galen Savidge <galensavidge@gmail.com>
2024-01-14 20:10:40 -0700

    Added diff viewer

--- init.lua
+++ init.lua
@@ -50,6 +50,15 @@ require("lazy").setup({
       'nvim-telescope/telescope.nvim',
     },
   },
+  { -- Virt-column
+    'lukas-reineke/virt-column.nvim',
+  },
+  { -- Gitsigns
+    'lewis6991/gitsigns.nvim',
+  },
+  { -- Diffview
+    'sindrets/diffview.nvim',
+  },
 })
 
 -- Set color theme
@@ -69,6 +78,10 @@ require('lualine').setup {
  -- Move vertically by visual line (gets around super long lines)
 vim.keymap.set({'n', 'x'}, 'j', 'gj')
 vim.keymap.set({'n', 'x'}, 'k', 'gk')
+
+-- Split panes
+vim.keymap.set('n', '<A-s>', '<C-w>s')
+vim.keymap.set('n', '<A-v>', '<C-w>v')
 
  -- Move between panes to left/bottom/top/right
 vim.keymap.set('n', '<C-h>', '<C-w>h')
@@ -99,13 +112,47 @@ vim.keymap.set('n', 'M', 'D')
 vim.keymap.set({'n', 'x'}, '<leader>m', 'm')
 vim.keymap.set('x', 'p', 'P')  -- Prevent yank on put in visual mode
 
--- Set file grep config
+ -- Set file grep config
 local ts = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', ts.find_files, {})
 vim.keymap.set('n', '<leader>f', ts.live_grep, {})
 vim.keymap.set('n', '<leader>b', ts.buffers, {})
 vim.keymap.set('n', '<leader>h', ts.help_tags, {})
-vim.keymap.set('n', '<leader>p', require('telescope').extensions.neoclip.plus, {})
+vim.keymap.set('n', '<leader>p', require('telescope').extensions.neoclip.plus)
+
+ -- Git integration
+local gs = require('gitsigns')
+vim.keymap.set('n', ']c', function()
+  if vim.wo.diff then return ']c' end
+    vim.schedule(function() gs.next_hunk() end)
+  return '<Ignore>'
+end, {expr=true})
+
+vim.keymap.set('n', '[c', function()
+  if vim.wo.diff then return '[c' end
+    vim.schedule(function() gs.prev_hunk() end)
+  return '<Ignore>'
+end, {expr=true})
+
+vim.keymap.set('n', '<leader>hs', gs.stage_hunk)
+vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
+vim.keymap.set('v', '<leader>hs', function()
+  gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')}
+end)
+vim.keymap.set('v', '<leader>hr', function()
+  gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')}
+end)
+vim.keymap.set('n', '<leader>hS', gs.stage_buffer)
+vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk)
+vim.keymap.set('n', '<leader>hR', gs.reset_buffer)
+vim.keymap.set('n', '<leader>hp', gs.preview_hunk)
+vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end)
+vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame)
+vim.keymap.set('n', '<leader>hd', gs.diffthis)
+vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end)
+vim.keymap.set('n', '<leader>td', gs.toggle_deleted)
+
+vim.keymap.set({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 
  -- Project/session management
 require("persisted").setup({
@@ -127,6 +174,13 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"},
  -- Set tab length by file type
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"},
 {
+  pattern = {"*.lua"},
+  callback = function(ev)
+    vim.opt.tabstop = 2
+  end
+})
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"},
+{
   pattern = {"*.rst"},
   callback = function(ev)
     vim.opt.tabstop = 3
@@ -143,6 +197,12 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     end,
 })
 
+ -- Better color column
+require("virt-column").setup()
+
+ -- Git integration (show hunks, stage hunks, blame)
+ require('gitsigns').setup()
+
  ---------- MISC SETTINGS ----------
 local opt = vim.opt
 opt.autoindent = true
@@ -151,6 +211,7 @@ opt.backup = false
 opt.breakindent = true
 opt.breakindentopt = "shift:2"
 opt.clipboard = "unnamedplus" -- Use system clipboard by default
+opt.colorcolumn = '80'
 opt.cursorline = true
 opt.expandtab = true
 opt.hlsearch = true
