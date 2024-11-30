1e5a8547e12aaac929fee3be95f76752f03e5997
Galen Savidge <galensavidge@gmail.com>
2024-11-29 20:19:51 -0700

    Cleanup

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -52,7 +52,6 @@ return {
         sections = {
           lualine_a = { 'mode' },
           lualine_b = { 'filename' },
-          -- lualine_c = { 'branch', 'diff', 'diagnostics' },
           lualine_c = { 'branch', 'diff', 'diagnostics' },
           lualine_x = { current_signature },
           lualine_y = { require('lsp-progress').progress, 'filetype' },
